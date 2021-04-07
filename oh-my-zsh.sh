5400d67d69a8a93a6b90dd0810e5081a39e99283
Shayne <shayne@oreokirby.dev>
2021-04-07 08:29:05 -0400

    Added oh-my-zsh.sh script

---
+++ oh-my-zsh.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Install zsh-syntax-highlighting plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install zsh-autosuggestions plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
