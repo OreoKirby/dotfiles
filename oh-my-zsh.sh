f03e9f41a5a29db639781c35bd216ba9aa1b576d
Shayne <shayne@oreokirby.dev>
2021-04-07 09:18:37 -0400

    oh-my-zsh.ah: added install for powerlevel10k theme

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -5,3 +5,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 # Install zsh-autosuggestions plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
