723d88df8bc649d605f91471539bc580e0e3bd66
Shayne <shayne@oreokirby.dev>
2021-05-23 11:11:06 -0400

    brew.sh: update existing macOS tools

--- brew.sh
+++ brew.sh
@@ -28,3 +28,11 @@ brew install wget --with-iri
 
 # Install GnuPG to enable PGP-signing commits.
 brew install gnupg
+
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+brew install screen
+brew install php
+brew install gmp
