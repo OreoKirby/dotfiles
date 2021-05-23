ae41631b047d221a255b061e6084b4a96195380d
Shayne <shayne@oreokirby.dev>
2021-05-23 15:07:28 -0400

    oh-my-zsh: install oh-my-zsh via curl

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Install oh-my-zsh via curl
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Install zsh-syntax-highlighting plugin
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
