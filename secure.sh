8f3d4f60568e406d9cb5c031816b40cb6eadd278
Shayne <shayne@oreokirby.dev>
2021-05-27 18:35:17 -0400

    secure.sh: add captive portal

--- secure.sh
+++ secure.sh
@@ -105,3 +105,18 @@ killall mds > /dev/null 2>&1
 sudo mdutil -i on / > /dev/null
 # Rebuild the index from scratch
 sudo mdutil -E / > /dev/null
+
+###############################################################################
+# Captive portal                                                              #
+###############################################################################
+
+# When macOS connects to new networks, it checks for Internet connectivity and
+# may launch a Captive Portal assistant utility application.
+
+# An attacker could trigger the utility and direct a Mac to a site with malware
+# without user interaction, so it's best to disable this feature and log in to
+# captive portals using your regular Web browser by navigating to a non-secure
+# HTTP page and accepting a redirect to the captive portal login interface
+# (after disabling any custom proxy or DNS settings).
+
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.captive.control.plist Active -bool false
