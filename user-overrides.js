b5ebaf83a737532606c9a94d28372861bc1b83a8
Shayne <shayne@oreokirby.dev>
2022-04-13 07:30:03 -0400

    Add firefox user-overrides.js

---
+++ user-overrides.js
@@ -0,0 +1,7 @@
+/*** MY OVERRIDES ***/
+user_pref("_user.js.parrot", "overrides section syntax error");
+
+user_pref("keyword.enabled", true); //0801: disable location bar using search
+user_pref("security.secure_connection_icon_color_gray", false); //SSL Connection Green Lock
+
+user_pref("_user.js.parrot", "overrides section successful");
